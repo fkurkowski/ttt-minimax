650445d8a6390665fa016d7ea777e8b13b70ae30
Felipe Kurkowski <felipekurkowski@gmail.com>
2017-02-08 16:35:34 -0200

    Functions to check whether the game is over and which player won.

--- src/Board.hs
+++ src/Board.hs
@@ -2,6 +2,7 @@
 module Board where
 
 import Data.List as List
+import Data.Maybe as Maybe
 
 -- Board size
 size = 3
@@ -27,9 +28,29 @@ chunksOf n xs = init : chunksOf n rest
 chunksOfBoardSize :: [a] -> [[a]]
 chunksOfBoardSize = chunksOf size
 
+combos :: [a] -> [[a]]
+combos board = rows ++ cols ++ diags
+  where rows = chunksOfBoardSize board
+        cols = transpose rows
+        diags = [[a1,ab2,a3], [b1,ab2,b3]]
+          where [[a1,_,b1],
+                 [_,ab2,_],
+                 [b3,_,a3]] = rows
+
 newBoard :: Board
 newBoard = replicate (size*size) Nothing
 
 move :: Player -> Position -> Board -> Board
 move p pos board = xs ++ (pure p : tail ys)
   where (xs, ys) = splitAt pos board
+
+winner :: Board -> Maybe Player
+winner board = do
+  let isWinningSet [a,b,c] = a == b && b == c && Maybe.isJust a
+  set <- find isWinningSet . combos $ board
+  head set
+
+gameOver :: Board -> Bool
+gameOver = (||) <$> isBoardFull <*> hasWinner
+  where isBoardFull = all isJust
+        hasWinner = isJust . winner
